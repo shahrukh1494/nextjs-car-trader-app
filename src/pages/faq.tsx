8bdee67bbbf64af04cde6f31b72d405213d39a8a
unknown <shahrukh1494@gmail.com>
2021-05-15 20:45:41 +0530

    faq page complete

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,3 +1,8 @@
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { GetStaticProps } from "next";
 import { FaqModel } from "../../api/Faq";
 import { openDB } from "../openDB";
@@ -10,9 +15,18 @@ export default function Faq({ faq }: FaqProps) {
   return (
     <div>
       {faq.map((f) => (
-        <div>
-          {f.question} | {f.answer}
-        </div>
+        <Accordion key={f.id}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>{f.question}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{f.answer}</Typography>
+          </AccordionDetails>
+        </Accordion>
       ))}
     </div>
   );
